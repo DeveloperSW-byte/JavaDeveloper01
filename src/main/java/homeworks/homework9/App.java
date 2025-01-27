e2540e3a675b63a641097edaac5050f2494a7b61
SergeyPyatunin <pyatunins.top@gmail.com>
2025-01-27 18:46:31 +0500

    Создание машин

--- src/main/java/homeworks/homework9/App.java
+++ src/main/java/homeworks/homework9/App.java
@@ -5,21 +5,10 @@ import homeworks.homework9.model.Car;
 public class App {
     public static void main (String [ ] args) {
 
-        Car car = new Car();
-        car.setBrand("BMW");
-        car.setAcceleration(200);
-        car.setHP(250);
-        car.setSuspension(200);
-        car.setDurability(2000);
-        car.setRelease(2020);
-
-        Car car1 = new Car ();
-        car.setBrand("Porche");
-        car.setAcceleration(225);
-        car.setHP(280);
-        car.setSuspension(180);
-        car.setDurability(2250);
-        car.setRelease(2019);
+        Car car = new Car("BMW", "M5", 2020, 250, 200, 2000, 200);
+        Car car1 = new Car ("Porsche", "GT6", 2019, 280, 225, 2250, 180);
+        Car car2 = new Car ("VAZ", "2107", 1980, 110, 100, 5000, 100);
+        PerfomanceCar perfomanceCar = new PerfomanceCar("Audi", "RS6", 2024, 425, 230, 2200, 200);
 
         Car car2 = new Car ();
         car.setBrand("VAZ");
