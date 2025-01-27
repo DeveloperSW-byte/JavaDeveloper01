f8d86ab07e83c44c24ec66dc86bb94bfe367836a
SergeyPyatunin <pyatunins.top@gmail.com>
2025-01-27 19:33:00 +0500

    Наследование от класса CAR

--- src/main/java/homeworks/homework9/model/PerfomanceCar.java
+++ src/main/java/homeworks/homework9/model/PerfomanceCar.java
@@ -2,4 +2,9 @@ package homeworks.homework9.model;
 
 public class PerfomanceCar extends Car{
 
+    public PerfomanceCar (String brand, String model, Integer release, Integer HP, Integer acceleration, Integer durability, Integer suspension){
+        super(brand, model, release, (int)(HP * 1.5), acceleration, durability, (int)(suspension / 0.25));
+        String[] addOns = new String[10];
+    }
+
 }
