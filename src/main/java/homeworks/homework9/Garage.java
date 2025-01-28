e0b98a2c1a64c8dda83358f016c551328b380b64
SergeyPyatunin <pyatunins.top@gmail.com>
2025-01-28 19:37:17 +0500

    Создание массива гараж и определение методов для добавления авто в гараж

--- src/main/java/homeworks/homework9/Garage.java
+++ src/main/java/homeworks/homework9/Garage.java
@@ -3,7 +3,16 @@ package homeworks.homework9;
 import homeworks.homework9.model.Car;
 
 public class Garage {
-    private Garage (Car[] cars){
+    private List<Car> parkedCars;
+
+    public Garage(){
+        this.parkedCars = new ArrayList<>();
+    }
+
+    public void parkCar(Car car) {
+        parkedCars.add(car);
+        System.out.println("Машина " + car.getBrand() + " " + car.getModel() + " припаркована в гараже.");
+    }
 
     }
 }
