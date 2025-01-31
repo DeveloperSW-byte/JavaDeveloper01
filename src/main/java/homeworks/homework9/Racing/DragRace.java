c25ab4da08f6d7ca4b62185aa7315f90ee187d80
SergeyPyatunin <pyatunins.top@gmail.com>
2025-01-31 21:41:06 +0500

    Наследование от класса Race

--- src/main/java/homeworks/homework9/Racing/DragRace.java
+++ src/main/java/homeworks/homework9/Racing/DragRace.java
@@ -2,7 +2,7 @@ package homeworks.homework9.Racing;
 
 public class DragRace extends Race{
 
-    public DragRace (Integer length, String route, Integer reward){
-        super(length, route, reward);
+    public DragRace (Integer length, String route, Integer reward, Object racers){
+        super(length, route, reward, racers);
     }
 }
