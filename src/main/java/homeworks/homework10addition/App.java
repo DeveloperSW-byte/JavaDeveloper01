c082dcd1a5a6d7a7c8496426882ef04f25a69bb0
SergeyPyatunin <pyatunins.top@gmail.com>
2025-02-05 00:18:32 +0500

    Код для компиляции услвой из ДЗ и его успешная компиляция

--- src/main/java/homeworks/homework10addition/App.java
+++ src/main/java/homeworks/homework10addition/App.java
@@ -9,6 +9,18 @@ public class App {
         Optional<String> optionalS = Optional.of(null);
 
         Pair<Integer, String> pair = Pair.of(1, "hello");
+        Integer i = pair.getFirst();
+        String s = pair.getSecond();
+
+
+        Pair<Integer, String> pair2 = Pair.of(1, "hello");
+        boolean mustBeTrue = pair.equals(pair2);
+        boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode();
+
+
+        System.out.println("Должны быть равны pair and pair2 = " + mustBeTrue);
+        System.out.println("Точной должны быть равны pair and pair2 = " + mustAlsoBeTrue);
+
 
     }
 }
