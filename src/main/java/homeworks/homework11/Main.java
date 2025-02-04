0da0cebdd0527d073adb6f1d6ea2ac33736c0793
SergeyPyatunin <pyatunins.top@gmail.com>
2025-02-04 21:12:14 +0500

    Создание нового обьекта из класса Car

--- src/main/java/homeworks/homework11/Main.java
+++ src/main/java/homeworks/homework11/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Car car1 = new Car(00001, "FORD", "RED", 20000, 15000);
+        Car car1 = new Car("a001oe", "FORD", "RED", 20000, 15000);
 
 
         System.out.println("Номер машины " + car1.getNumber() +  " Новая машина " +car1.getModel() + " Цвет машины " +
