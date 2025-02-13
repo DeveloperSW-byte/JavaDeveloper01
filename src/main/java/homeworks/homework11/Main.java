815ffe5fd01f9824fa39e2114c612952fad3d76d
SergeyPyatunin <pyatunins.top@gmail.com>
2025-02-13 19:30:07 +0500

    Создание автомобилей

--- src/main/java/homeworks/homework11/Main.java
+++ src/main/java/homeworks/homework11/Main.java
@@ -3,6 +3,18 @@ package homeworks.homework11;
 public class Main {
 
     public static void main(String[] args) {
+        Car[] cars = new Car[10];
+
+        cars[0] = new Car("a123me", "Mercedes", "White", 0, 8300000);
+        cars[1] = new Car("b873of", "Volga", "Black", 0, 673000);
+        cars[2] = new Car("w487mn", "Lexus", "Grey", 76000, 900000);
+        cars[3] = new Car("p987hj", "Volga", "Red", 610, 704340);
+        cars[4] = new Car("c987ss", "Toyota", "White", 254000, 761000);
+        cars[5] = new Car("o983op", "Toyota", "Black", 698000, 740000);
+        cars[6] = new Car("p146op", "BMW", "White", 271000, 850000);
+        cars[7] = new Car("u893ii", "Toyota", "Purple", 210900, 440000);
+        cars[8] = new Car("I097df", "Toyota", "black", 108000, 780000);
+        cars[9] = new Car("y876wd", "Toyota", "Black", 160000, 1000000);
 
         Car car1 = new Car("a123me", "Mercedes", "White", 0, 8300000);
         Car car2 = new Car("b873of", "Volga", "Black", 0, 673000);
